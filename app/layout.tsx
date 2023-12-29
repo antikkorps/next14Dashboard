cba4c1049d4c7fd38a75af0426c0ce4702ba2cfe
Franck <franckvienot7@gmail.com>
2023-12-29 12:22:13 +0100

    added login && metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Next14 Dashboard',
+    default: 'Next14 Dashboard',
+  },
+  description: 'A simple invoicing app built with Next.js',
+  metadataBase: new URL(
+    'https://next14-dashboard-mf3csygkk-antikkorps.vercel.app',
+  ),
+};
 
 export default function RootLayout({
   children,
